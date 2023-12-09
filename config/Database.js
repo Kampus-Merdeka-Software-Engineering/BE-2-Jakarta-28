d80a0555f62a3c7d204a847672b453ba0fb41343
Michael Sudirgo <michaelsudirgo2@gmail.com>
2023-12-10 06:49:01 +0700

    Initialize Database for railway

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
 
 // Initialisasi database baru dengan nama code_blog dan usernya root tanpa ada password
-const db = new Sequelize(`code_blog`, 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+// const db = new Sequelize(`code_blog`, 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+
+// Initialisasi mysql railway
+const db = new Sequelize("railway", "root", "adHhDbEaB3he22gGdh1cHadG5h45-b3E", {
+    host: "monorail.proxy.rlwy.net",
+    dialect: "mysql"
 });
 
 export default db;
